67ec89188cbda9359e0c7b0da7609d5475f118ff
xiongxiaoyang <773861846@qq.com>
2021-01-04 20:54:02 +0800

    elasticsearch升级到7.x，集成RestHighLevelClient客户端

--- novel-search/src/main/java/com/java2nb/novel/SearchApplication.java
+++ novel-search/src/main/java/com/java2nb/novel/SearchApplication.java
@@ -2,6 +2,7 @@ package com.java2nb.novel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@ServletComponentScan
 public class SearchApplication {
 
     public static void main(String[] args) {
